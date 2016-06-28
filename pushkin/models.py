97e848d84356c9de2049001e064438ab5da0917a
Ilya Berestenko <ilique@digitalmanufactory.ru>
2016-06-28 16:03:33 +0300

    make command text not unique

--- pushkin/models.py
+++ pushkin/models.py
@@ -56,7 +56,7 @@ class CommandArgument(models.Model):
 
 
 class Command(models.Model):
-    text = models.CharField(max_length=255, unique=True)
+    text = models.CharField(max_length=255)
     arguments = models.ManyToManyField(CommandArgument, blank=True)
 
     def __str__(self):
